eb85f6823d73a1bdb4d3da9217f846e9076830de
Nemuel Rico <nemuel1337@gmail.com>
2022-03-05 17:42:18 +0800

    Try it Yourself

---
+++ Chapter 10/10-1 Learning Python.py
@@ -0,0 +1,19 @@
+#reading contents of a file
+with open('learning_python.txt') as file:
+    contents = file.read()
+print(contents.rstrip())
+
+
+#reading line by line
+filename = 'learning_python.txt'
+print("----------------------------")
+with open(filename) as object:
+    for line in object:
+        print(line.rstrip())
+print("----------------------------")
+#making a list of liens from a file.
+with open(filename) as object:
+    lists = object.readlines()
+
+for line in lists:
+    print(line.rstrip())
