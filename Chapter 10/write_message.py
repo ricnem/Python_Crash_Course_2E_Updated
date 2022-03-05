51621ab398507afd245c49ed84dbec10814ccede
Nemuel Rico <nemuel1337@gmail.com>
2022-03-05 18:13:39 +0800

    Try it Yourself

--- Chapter 10/write_message.py
+++ Chapter 10/write_message.py
@@ -1,10 +1,14 @@
 filename = 'nem.txt'
 
+# opening a file in write mode using 'w'
 with open(filename, 'w') as file_object:
-    file_object.write("Learn from your setbacks!")
+    file_object.write("Learn from your setbacks!\n")
 
     x = 5
     while x > 0:
-        file_object.write("\nParang awa mo na...")
+        file_object.write("\nParang awa mo na...\n")
         x -= 1
 
+# opening a file in append mode using 'a'
+with open(filename, 'a') as file_object:
+    file_object.write("\nTranscend your self!")
