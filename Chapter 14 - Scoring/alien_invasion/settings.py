bcbf3a69234fb49938b0bde4844c284f2777b6e8
Nemuel <nemuel1337@gmail.com>
2022-06-05 00:59:02 +0800

    First Project ni Nemuel :)

---
+++ Chapter 14 - Scoring/alien_invasion/settings.py
@@ -0,0 +1,24 @@
+class Settings:
+    """A class to store all settings for Alien Invasion"""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen settings
+        self.screen_width = 1920
+        self.screen_height = 1080
+        self.bg_color = (230, 230, 230)
+        self.ship_speed = 1.5
+        self.ship_limit = 1
+
+        # Bullet settings
+        self.bullet_speed = 1.5
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 10
+
+        # Alien settings
+        self.alien_speed = 1
+        self.fleet_drop_speed = 10
+        # fleet direction of 1 represents right; -1 represents left.
+        self.fleet_direction = -1
