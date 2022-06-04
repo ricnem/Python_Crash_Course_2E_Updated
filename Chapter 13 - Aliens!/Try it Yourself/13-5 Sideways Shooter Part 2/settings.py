c6cc18745744c4ccbb49dec207a3a6914e49c1d6
Nemuel <nemuel.rico.palomo@adamson.edu.ph>
2022-06-04 21:51:12 +0800

    First Project ni Nemuel :)

--- Chapter 13 - Aliens!/Try it Yourself/13-5 Sideways Shooter Part 2/settings.py
+++ Chapter 13 - Aliens!/Try it Yourself/13-5 Sideways Shooter Part 2/settings.py
@@ -1,23 +1,25 @@
 class Settings:
-    """A class to store all settings for Alien Invasion"""
+    """A class to store all settings for Sideways Shooter."""
 
     def __init__(self):
         """Initialize the game's settings."""
         # Screen settings
-        self.screen_width = 1920
-        self.screen_height = 1080
+        self.screen_width = 1200
+        self.screen_height = 800
         self.bg_color = (230, 230, 230)
-        self.ship_speed = 1.5
+
+        # Ship settings
+        self.ship_speed = 3.0
 
         # Bullet settings
-        self.bullet_speed = 1.5
-        self.bullet_width = 3
-        self.bullet_height = 15
+        self.bullet_speed = 6.0
+        self.bullet_width = 15
+        self.bullet_height = 3
         self.bullet_color = (60, 60, 60)
-        self.bullets_allowed = 10
+        self.bullets_allowed = 3
 
-        # Alien settings
-        self.alien_speed = 0.5
-        self.fleet_drop_speed = 2
-        # fleet direction of 1 represents right; -1 represents left.
-        self.fleet_direction = -1
+        # Alien settings.
+        #  alien_frequency controls how often a new alien appear.s
+        #    Higher values -> more frequent aliens. Max = 1.0.
+        self.alien_frequency = 0.008
+        self.alien_speed = 1.5
--- Chapter 13 - Aliens!/Try it Yourself/13-5 Sideways Shooter Part 2/ship.py
+++ Chapter 13 - Aliens!/Try it Yourself/13-5 Sideways Shooter Part 2/ship.py
@@ -1,38 +1,39 @@
 import pygame
 
+
 class Ship:
     """A class to manage the ship."""
 
-    def __init__(self, ai_game):
-        """Initialize the ship and set its starting position,"""
-        self.screen = ai_game.screen
-        self.settings = ai_game.settings
-        self.screen_rect = ai_game.screen.get_rect()
+    def __init__(self, ss_game):
+        """Initialize the ship and set its starting position."""
+        self.screen = ss_game.screen
+        self.settings = ss_game.settings
+        self.screen_rect = ss_game.screen.get_rect()
 
         # Load the ship image and get its rect.
-        self.image = pygame.image.load('images/ship.bmp')
+        self.image = pygame.image.load('images/rocket_small.png')
         self.rect = self.image.get_rect()
 
-        # Start each new ship at the bottom center of the screen.
-        self.rect.midbottom = self.screen_rect.midbottom
+        # Start each new ship at the center of the left side of the screen.
+        self.rect.midleft = self.screen_rect.midleft
 
-        # Store a decimal value for the ship's horizontal position.
-        self.x = float(self.rect.x)
+        # Store a decimal value for the ship's vertical position.
+        self.y = float(self.rect.y)
 
-        # Movement flag
-        self.moving_right = False
-        self.moving_left = False
+        # Movement flags
+        self.moving_up = False
+        self.moving_down = False
 
     def update(self):
-        """Update the ship's position based on the movement flag."""
-        if self.moving_right and self.rect.right < self.screen_rect.right:
-            self.x += self.settings.ship_speed
-        if self.moving_left and self.rect.left > 0:
-            self.x -= self.settings.ship_speed
+        """Update the ship's position based on movement flags."""
+        # Update the ship's y value, not the rect.
+        if self.moving_up and self.rect.top > 0:
+            self.y -= self.settings.ship_speed
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y += self.settings.ship_speed
 
-        # Update rect object from self.x
-        self.rect.x = self.x
-
+        # Update rect object from self.y.
+        self.rect.y = self.y
 
     def blitme(self):
         """Draw the ship at its current location."""
